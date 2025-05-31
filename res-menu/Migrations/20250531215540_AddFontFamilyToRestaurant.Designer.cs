342747c129d9eadfe8404f198b476c582b7b1b4d
wodyjowski <mkisiel58@gmail.com>
2025-05-31 23:56:42 +0200

    Restaurant fix

--- res-menu/Migrations/20250531215540_AddFontFamilyToRestaurant.Designer.cs
+++ res-menu/Migrations/20250531215540_AddFontFamilyToRestaurant.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using res_menu.Data;
@@ -11,9 +12,11 @@ using res_menu.Data;
 namespace res_menu.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250531215540_AddFontFamilyToRestaurant")]
+    partial class AddFontFamilyToRestaurant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ res-menu/Migrations/20250531215540_AddFontFamilyToRestaurant.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace res_menu.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFontFamilyToRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FontFamily",
+                table: "Restaurants",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FontFamily",
+                table: "Restaurants");
+        }
+    }
+}
